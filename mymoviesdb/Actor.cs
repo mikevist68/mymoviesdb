1a9a87e93b32bb7eb1f9999ae68f80c0443e485e
Michael Vistocci <mikevist68@yahoo.com>
2023-06-26 15:37:10 -0400

    Add project files.

---
+++ mymoviesdb/Actor.cs
@@ -0,0 +1,13 @@
+﻿namespace mymoviesdb
+{
+    public class Actor
+    {
+        public class Actor
+        {
+            public int ActorId { get; set; }
+            public string Name { get; set; }
+            public DateTime Birthdate { get; set; }
+            public string Nationality { get; set; }
+        }
+    }
+}
---
+++ mymoviesdb/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace mymoviesdb
+{
+    public class Movie
+    {
+        public class Movie
+        {
+            public int MovieId { get; set; }
+            public string Title { get; set; }
+            public string Genre { get; set; }
+            public int ReleaseYear { get; set; }
+            public string Director { get; set; }
+        }
+    }
+}
